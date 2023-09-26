d54e062fd404b3591cf274abd395a611cf3bbfbd
Hong Yong Ching <venushong667@gmail.com>
2023-09-26 20:02:06 +0800

    Add weather API

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { WeatherService } from './weather.service';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Duration, WeatherService } from './weather.service';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('weather')
 export class WeatherController {
     constructor(private readonly weatherService: WeatherService) {}
 
     @Get()
-    getWeatherForecast() {
-        return this.weatherService.getWeatherForecast();
+    async findAll(
+        @Query('duration') duration: Duration,
+        @Query('date_time') datetime?: string,
+        @Query('date') date?: string
+    ) {
+        // date_time format: YYYY-MM-DD[T]HH:mm:ss (SGT)
+        // date format: YYYY-MM-DD
+        
+        return await this.weatherService.getWeatherForecast(duration, datetime, date);
     }
     
 }
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-import { catchError, firstValueFrom, map } from 'rxjs';
+import { catchError, filter, firstValueFrom, map } from 'rxjs';
 import { AxiosError } from 'axios';
 import { APIStatus, Coordinate } from 'src/geo/geo.service';
 
@@ -47,7 +47,7 @@ export class WeatherService {
         this.weatherEndpoint = join(configService.get("gov_api.url"), 'environment');
     }
 
-    getWeatherForecast(duration: Duration = '2-hour', datetime?: Date, date?: string) {
+    getWeatherData(duration: Duration = '2-hour', datetime?: string, date?: string) {
         // date format: YYYY-MM-DD[T]HH:mm:ss (SGT)
         // date format: YYYY-MM-DD
         const params = {};
@@ -60,9 +60,21 @@ export class WeatherService {
         return this.httpService.get(join(this.weatherEndpoint, `${duration}-weather-forecast`), { params: params });
     }
 
+    async getWeatherForecast(duration: Duration = '2-hour', datetime?: string, date?: string) {
+        return await firstValueFrom(
+            this.getWeatherData(duration, datetime, date).pipe(
+                map(res => res.data.items),
+                catchError((error: AxiosError) => {
+                    this.logger.error(error.response.data);
+                    throw 'An error happened!';
+                })
+            )
+        )
+    }
+
     async getAreaMetadata() {
         return await firstValueFrom(
-            this.getWeatherForecast().pipe(
+            this.getWeatherData().pipe(
                 map(res => res.data.area_metadata as AreaMetadata[]),
                 catchError((error: AxiosError) => {
                     this.logger.error(error.response.data);
