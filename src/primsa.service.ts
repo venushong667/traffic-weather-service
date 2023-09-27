67178f29ac585bef6fd0f773f29eefb0c9d8c0ae
Hong Yong Ching <venushong667@gmail.com>
2023-09-28 00:50:41 +0800

    Add prisma destroy method

--- src/primsa.service.ts
+++ src/primsa.service.ts
@@ -1,10 +1,14 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 // onModuleInit is optional — if you leave it out, Prisma will connect lazily on its first call to the database.
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     async onModuleInit() {
         await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
