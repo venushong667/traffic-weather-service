4a0b7db18436cdf167c99cbf03b9aee7915a236a
Hong Yong Ching <venushong667@gmail.com>
2023-09-28 00:49:32 +0800

    Add controller test cases

--- src/traffic/traffic.controller.spec.ts
+++ src/traffic/traffic.controller.spec.ts
@@ -1,20 +1,156 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TrafficController } from './traffic.controller';
+import { Traffic, TrafficController } from './traffic.controller';
 import { TrafficService } from './traffic.service';
+import { ConfigModule } from '@nestjs/config';
+import { GeoService } from '../geo/geo.service';
+import { of } from 'rxjs';
+import { CameraMetadata, TrafficItem } from 'src/geo/interfaces';
 
 describe('TrafficController', () => {
-    let controller: TrafficController;
+    let trafficController: TrafficController;
+
+    const mockTrafficService = {
+        getCameras: jest.fn(({ where: { id: { in: [id] } } }) => {
+            if (id === "unseen_cam_id") {
+                return []
+            }
+            
+            return [{
+                id: "1",
+                latitude: 1,
+                longitude: 1,
+                address: "address",
+                route: "route",
+                neighborhood: "neighborhood",
+                region: "central",
+            }]
+            
+        }),
+        createCamera: jest.fn((data) => {
+            return {
+                id: '1',
+                ...data
+            }
+        })
+    }
+
+    const mockGeoService = {
+        getTrafficData: jest.fn((datetime) => {
+            return of([{
+                timestamp: new Date().toISOString().split('.')[0],
+                cameras: [{
+                    timestamp: new Date().toISOString().split('.')[0],
+                    image: "image_url",
+                    location: {
+                        longitude: 1,
+                        latitude: 1
+                    },
+                    camera_id: datetime === new Date("2023-09-07").toISOString().split('.')[0] ? "unseen_cam_id" : "1" ,
+                    image_metadata: {
+                        height: 1,
+                        width: 1,
+                        md: "md"
+                    }
+                }] satisfies CameraMetadata[] as CameraMetadata[]
+            }] satisfies TrafficItem[] as TrafficItem[])
+            
+        }),
+        getCameraLocation: jest.fn((cameras: CameraMetadata[]) => {
+            const promises = cameras.map(async (cam) => {
+                return {
+                    ...cam,
+                    address: 'address',
+                    route: 'route',
+                    neighborhood: 'neighborhood',
+                    region: 'central'
+                }
+            })
+            return Promise.all(promises);
+        })
+    }
+
+    const mockConfigService = {
+        gov_api: {
+            url: 'gov_api_key'
+        }
+    }
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forFeature(async () => (mockConfigService))
+            ],
             controllers: [TrafficController],
-            providers: [TrafficService],
-        }).compile();
+            providers: [
+                TrafficService,
+                GeoService
+            ],
+        })
+            .overrideProvider(TrafficService)
+            .useValue(mockTrafficService)
+            .overrideProvider(GeoService)
+            .useValue(mockGeoService)
+            .compile();
 
-        controller = module.get<TrafficController>(TrafficController);
+        trafficController = module.get<TrafficController>(TrafficController);
     });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
-    });
+    describe('findAll', () => {
+        it('seen camera data should not store and return directly', async () => {
+            const datetime = "other date";
+            const result = [{
+                id: '1',
+                address: expect.any(String),
+                route: expect.any(String),
+                neighborhood: expect.any(String),
+                region: expect.any(String),
+                image: {
+                    url: expect.any(String),
+                    metadata: {
+                        height: expect.any(Number),
+                        width: expect.any(Number),
+                        md: expect.any(String)
+                    },
+                },
+                timestamp: expect.any(String)
+            }] satisfies Traffic[] as Traffic[]
+
+            expect(await trafficController.findAll(datetime)).toEqual(result);
+
+            expect(mockGeoService.getTrafficData).toHaveBeenCalled();
+            expect(mockTrafficService.getCameras).toHaveBeenCalled();
+            // functions used to store data should not be called
+            expect(mockGeoService.getCameraLocation).not.toHaveBeenCalled();
+            expect(mockTrafficService.createCamera).not.toHaveBeenCalled();
+        })
+
+        it('should store unseen camera location data and return it', async () => {
+            const datetime = new Date("2023-09-07").toISOString().split('.')[0];
+            const result = [{
+                id: 'unseen_cam_id',
+                address: expect.any(String),
+                route: expect.any(String),
+                neighborhood: expect.any(String),
+                region: expect.any(String),
+                image: {
+                    url: expect.any(String),
+                    metadata: {
+                        height: expect.any(Number),
+                        width: expect.any(Number),
+                        md: expect.any(String)
+                    },
+                },
+                timestamp: expect.any(String)
+            }] satisfies Traffic[] as Traffic[]
+
+            expect(await trafficController.findAll(datetime)).toEqual(result);
+
+            expect(mockGeoService.getTrafficData).toHaveBeenCalled();
+            expect(mockTrafficService.getCameras).toHaveBeenCalled();
+            expect(mockGeoService.getCameraLocation).toHaveBeenCalled();
+            expect(mockTrafficService.createCamera).toHaveBeenCalled();
+        })
+
+        
+    })
 });
--- src/weather/weather.controller.spec.ts
+++ src/weather/weather.controller.spec.ts
@@ -1,20 +1,119 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { AreaForecast, DailyForecast, DateForecast, FutureForecast, HourlyForecast, Period } from './interfaces';
 
 describe('WeatherController', () => {
-    let controller: WeatherController;
+    let weatherController: WeatherController;
+    let weatherService: WeatherService;
+
+    const mockConfigService = {
+        gov_api: {
+            url: 'gov_api_key'
+        }
+    }
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forFeature(async () => (mockConfigService)),
+                HttpModule
+            ],
             controllers: [WeatherController],
             providers: [WeatherService],
         }).compile();
 
-        controller = module.get<WeatherController>(WeatherController);
+        weatherService = module.get<WeatherService>(WeatherService);
+        weatherController = module.get<WeatherController>(WeatherController);
     });
 
-    it('should be defined', () => {
-        expect(controller).toBeDefined();
+    describe('findAll', () => {
+        it('should return result of 2-hour forecast', async () => {
+            const forecasts = [{
+                area: 'area1',
+                forecast: 'Windy'
+            } satisfies AreaForecast];
+
+            const result = [{
+                update_timestamp: expect.any(String),
+                timestamp: expect.any(String),
+                valid_period: {
+                    start: expect.any(String),
+                    end: expect.any(String)
+                },
+                forecasts: expect.arrayContaining(forecasts)
+            }] satisfies HourlyForecast[] as HourlyForecast[];
+
+            jest.spyOn(weatherService, 'getWeatherForecast').mockImplementation(async () => result);
+            
+            expect(await weatherController.findAll('2-hour')).toEqual(result);
+
+            expect(weatherService.getWeatherForecast).toHaveBeenCalledWith('2-hour', undefined, undefined)
+        });
+
+        it('should return result of 24-hour forecast', async () => {
+            const periods = [{
+                time: {
+                    start: expect.any(String),
+                    end: expect.any(String)
+                },
+                regions: {
+                    west: 'Windy'
+                } 
+            }] satisfies Period[] as Period[];
+
+            const result = [{
+                update_timestamp: expect.any(String),
+                timestamp: expect.any(String),
+                valid_period: {
+                    start: expect.any(String),
+                    end: expect.any(String)
+                },
+                periods: expect.arrayContaining(periods)
+            }] as DailyForecast[];
+
+            jest.spyOn(weatherService, 'getWeatherForecast').mockImplementation(async () => result);
+            
+            expect(await weatherController.findAll('24-hour')).toEqual(result);
+
+            expect(weatherService.getWeatherForecast).toHaveBeenCalledWith('24-hour', undefined, undefined)
+        });
+        
+        it('should return result of 4-day forecast', async () => {
+            const forecast1 = [{
+                date: expect.any(String),
+                forecast: expect.any(String),
+                timestamp: expect.any(String),
+                relative_humidity: {
+                    low: expect.any(Number),
+                    high: expect.any(Number),
+                },
+                temperature: {
+                    low: expect.any(Number),
+                    high: expect.any(Number),
+                },
+                wind: {
+                    speed: {
+                        low: expect.any(Number),
+                        high: expect.any(Number),
+                    },
+                    direction: expect.any(String)
+                }
+            }] satisfies DateForecast[] as DateForecast[];
+
+            const result = [{
+                update_timestamp: expect.any(String),
+                timestamp: expect.any(String),
+                forecasts: expect.arrayContaining(forecast1)
+            }] as FutureForecast[];
+
+            jest.spyOn(weatherService, 'getWeatherForecast').mockImplementation(async () => result);
+            
+            expect(await weatherController.findAll('4-day')).toEqual(result);
+
+            expect(weatherService.getWeatherForecast).toHaveBeenCalledWith('4-day', undefined, undefined)
+        });
     });
 });
