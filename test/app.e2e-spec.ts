cc79a6f36107d202b5c164b85533017fdfeca32d
Hong Yong Ching <venushong667@gmail.com>
2023-09-29 09:01:38 +0800

    Update docker-compose & remove unused test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
 
 import { AppModule } from './../src/app.module';
 
@@ -16,10 +15,4 @@ describe('AppController (e2e)', () => {
         await app.init();
     });
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!');
-    });
 });
